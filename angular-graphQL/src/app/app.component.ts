5262c0a0a303fa57cb40bb944ef3e545109217f4
Дмитрий Глотов <glotovdu@it2g.ru>
2023-07-14 02:12:54 +0300

    angular changes

--- angular-graphQL/src/app/app.component.ts
+++ angular-graphQL/src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'angular-graphQL';
 
-  public asd() {
-    return () => 'asd';
+  constructor(private apollo: Apollo) {}
+
+  public ngOnInit(): void {
+    this.graphqlUpdate();
+  }
+
+  public update(): void {
+    this.graphqlUpdate();
+  }
+
+  private graphqlUpdate(): void {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            fact {
+              fact
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        console.log(result.data);
+      });
   }
 }
--- angular-graphQL/src/app/app.module.ts
+++ angular-graphQL/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, ApolloModule, HttpClientModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:4000/',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
